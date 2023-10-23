2d20faa4b9a9c3aba64430217667041e01ff2673
Pal_314git status git initgit status git initgit add *lslslslsls <4130.stkabirdin@gmail.com>
2023-10-23 21:36:31 +0530

    PriorityQueue Implementation with insert and pop

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
+#include<stdio.h> 
+#include<stdlib.h>
 #include "util.h"
+#include "priorityq.h"
 
 int main(void)
 {
-  printf("Hello, World!\n");
+  PriorityQueue* pq = createPriorityQueue(100);
+  Pair test1 = { 1,5 };
+  Pair test2 = { 0,10 };
+  Pair test3 = { 3,1 };
+  insert(pq, test1);
+  insert(pq, test2);
+  insert(pq, test3);
+  for(int i = 0;i< 3;i++){
+    Pair top = gettop(pq);
+    printf("(%d, %d)\n", top.first, top.second);
+  }
+  free(pq->array);
+  free(pq);
 }
--- src/priorityq.h
+++ src/priorityq.h
@@ -1,50 +1,85 @@
 #include<stdio.h> 
 #include<stdlib.h>
 
-struct Pair
+typedef struct
 {
     /* data */
     int first;
     int second;
-};
+}Pair;
 
-struct PriorityQueue
+typedef struct
 {
     /* data */
-    Pair *array;
+    Pair* array;
     int size;
     int capacity;
-};
+} PriorityQueue;
 
-PriorityQueue *createPriorityQueue(int capacity){
+PriorityQueue* createPriorityQueue(int capacity) {
     PriorityQueue* pq = (PriorityQueue*)malloc(sizeof(PriorityQueue));
     pq->capacity = capacity;
     pq->size = 0;
-    pq->array = (Pair*)malloc(sizeof(Pair)*capacity);
+    pq->array = (Pair*)malloc(sizeof(Pair) * capacity);
+    return pq;
 }
 
-void heapify(PriorityQueue *pq, int ind){
-    int left = 2*ind + 1;
-    int right = 2*ind + 2;
+void swap(Pair* a, Pair* b) {
+    Pair temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(PriorityQueue* pq, int ind) {
+    int left = 2 * ind + 1;
+    int right = 2 * ind + 2;
     int smallest = ind;
-    if(left < pq->size && pq->array[left].second < pq->array[smallest].second){
+    if (left < pq->size && pq->array[left].second < pq->array[smallest].second) {
         smallest = left;
     }
-    if(right < pq->size && pq->array[right].second < pq->array[smallest].second){
+    if (right < pq->size && pq->array[right].second < pq->array[smallest].second) {
         smallest = right;
     }
-    if(smallest != ind){ 
-        Pair temp = pq->array[ind]; //swapping unconventional
-        pq->array[ind] = pq->array[smallest];
-        pq->array[smallest] = temp;
-        heapify(pq,smallest);
+    if (smallest != ind) {
+        swap(&pq->array[ind], &pq->array[smallest]);
+        heapify(pq, smallest);
     }
 }
 
-void insert(PriorityQueue *pq, Pair data){
-    if(pq->size == pq->capacity){
+// Insertion in Priority Queue
+void insert(PriorityQueue* pq, Pair data) {
+    if (pq->size == pq->capacity) {
         printf("Memory Full");
         return;
     }
-    
+    int i = pq->size;
+    pq->array[i] = data;
+    pq->size++;
+    while (i > 0 && pq->array[i].second < pq->array[(i - 1) / 2].second) {
+        swap(&pq->array[i], &pq->array[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+}
+
+//Check if pq is empty
+int Empty(PriorityQueue* pq) {
+    return (pq->size == 0);
+}
+
+// Get top element and pop operation
+Pair gettop(PriorityQueue* pq) {
+    if (pq->size <= 0) {
+        printf("Empty");
+        Pair empty = { 0,0 };
+        return empty;
+    }
+    if (pq->size == 1) {
+        pq->size--;
+        return pq->array[0];
+    }
+    Pair root = pq->array[0];
+    pq->array[0] = pq->array[pq->size - 1];
+    pq->size--;
+    heapify(pq, 0);
+    return root;
 }
