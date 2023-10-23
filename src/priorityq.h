2457e8fcfcc851f2491e89d5ddfee24cbc4f912f
Pal_314git status git initgit status git initgit add *lslslslsls <4130.stkabirdin@gmail.com>
2023-10-23 15:57:31 +0530

    PriorityQueue creation and heapify

--- src/priorityq.h
+++ src/priorityq.h
@@ -1,16 +1,50 @@
-struct Node
+#include<stdio.h> 
+#include<stdlib.h>
+
+struct Pair
 {
     /* data */
-    int key;
-    int data1;
-    int data2;
+    int first;
+    int second;
 };
 
-struct PriorityQ
+struct PriorityQueue
 {
     /* data */
+    Pair *array;
     int size;
-    Node *heap;
+    int capacity;
 };
 
+PriorityQueue *createPriorityQueue(int capacity){
+    PriorityQueue* pq = (PriorityQueue*)malloc(sizeof(PriorityQueue));
+    pq->capacity = capacity;
+    pq->size = 0;
+    pq->array = (Pair*)malloc(sizeof(Pair)*capacity);
+}
 
+void heapify(PriorityQueue *pq, int ind){
+    int left = 2*ind + 1;
+    int right = 2*ind + 2;
+    int smallest = ind;
+    if(left < pq->size && pq->array[left].second < pq->array[smallest].second){
+        smallest = left;
+    }
+    if(right < pq->size && pq->array[right].second < pq->array[smallest].second){
+        smallest = right;
+    }
+    if(smallest != ind){ 
+        Pair temp = pq->array[ind]; //swapping unconventional
+        pq->array[ind] = pq->array[smallest];
+        pq->array[smallest] = temp;
+        heapify(pq,smallest);
+    }
+}
+
+void insert(PriorityQueue *pq, Pair data){
+    if(pq->size == pq->capacity){
+        printf("Memory Full");
+        return;
+    }
+    
+}
