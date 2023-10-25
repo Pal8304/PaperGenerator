a797cec43e56bf80997cda1e7ecc61b933d21300
Nirlep5252 <nirlep5252@proton.me>
2023-10-25 17:27:35 +0530

    feat: Generate PDFs

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define UTILS_IMPLEMENTATION
 #include "util.h"
 
 #define PRIORITY_QUEUE_IMPLEMENTATION
 #include "priority_queue.h"
+
+#include "../external_libraries/pdfgen.h"
+#include "../external_libraries/pdfgen.c"
 
 int main(void)
 {
@@ -62,6 +66,43 @@ int main(void)
     Question *question = getDynamicArray(i, questionPaper);
     printLeftRight(question->question, question->difficulty, 0);
   }
+
+  char *pdf_title = NULL;
+  asprintf(&pdf_title, "Question Paper %d", (int)time(NULL));
+  struct pdf_info info = {
+      .creator = "Nirma University",
+      .producer = "Nirma University",
+      .title = {*pdf_title},
+      .author = "Nirma University",
+      .subject = "Question Paper",
+      .date = "Today"};
+  struct pdf_doc *pdf = pdf_create(PDF_A4_WIDTH, PDF_A4_HEIGHT, &info);
+  pdf_set_font(pdf, "Times-Roman");
+  pdf_append_page(pdf);
+
+  pdf_add_text(pdf, NULL, "Question Paper", 32, 50, PDF_A4_HEIGHT - 40, PDF_BLACK);
+  float temp;
+  float gap = 62;
+  for (int i = 0; i < questionPaper->size; ++i)
+  {
+    Question *question = getDynamicArray(i, questionPaper);
+    char *questionText = NULL;
+    asprintf(&questionText, "%d. %s", i + 1, question->question);
+    int y = PDF_A4_HEIGHT - gap;
+    pdf_add_text_wrap(pdf, NULL, questionText, 12, 50, y, 0, PDF_BLACK, PDF_A4_WIDTH - 150, PDF_ALIGN_LEFT, &temp);
+    pdf_add_text(pdf, NULL, question->difficulty, 12, PDF_A4_WIDTH - 50, y, PDF_BLACK);
+    gap += 10 + temp;
+    temp = 0;
+    free(questionText);
+  }
+
+  char *fileName = NULL;
+  asprintf(&fileName, "papers/%s.pdf", pdf_title);
+  pdf_save(pdf, fileName);
+  pdf_destroy(pdf);
+
+  free(fileName);
+  free(pdf_title);
 
   deserializePriorityQueue(easyPriorityQueue, "easy");
   deserializePriorityQueue(mediumPriorityQueue, "medium");
