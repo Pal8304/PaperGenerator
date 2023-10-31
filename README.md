b1a68c6002af55ddfa17d8675aa9ddcc73d6ced9
Nirlep5252 <nirlep5252@proton.me>
2023-10-31 09:56:15 +0530

    Fix repetition bug, improve PDF style

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ My and Palash's submission for the Data Structures assignment of Semester 3.
 Design and develop a question paper formation tool using an efficient data structure. Create three categories for pool of questions namely EASY, MEDIUM and HARD and store in separate files with 25 questions in each category for different topics of data structure subject. System should select questions randomly based on the count and difficulty level entered by the user from each category. Finally, your system should generate a question paper (Nirma University Format) for specified marks entered by user.
 
 ### **Constraints:**
+
 - No question(s) must be repeated in the generated paper.
 - There must be less similarity between multiple variants of papers generated.
 
--- src/main.c
+++ src/main.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define UTILS_IMPLEMENTATION
+#include "util.c"
 #include "util.h"
 
-#define PRIORITY_QUEUE_IMPLEMENTATION
 #include "priority_queue.h"
 
 #include "../external_libraries/pdfgen.h"
@@ -30,6 +29,17 @@ int main(void)
     return 1;
   }
 
+  if (questionCount > 75)
+  {
+    printf("ERROR: Questions count can't be greater than 75\n");
+    return 1;
+  }
+  if (easy > 25 || medium > 25 || hard > 25)
+  {
+    printf("ERROR: Questions from any category can't be greater than 25\n");
+    return 1;
+  }
+
   DynamicArray *easyQuestions = getQuestionsDifficulty("easy");
   DynamicArray *mediumQuestions = getQuestionsDifficulty("medium");
   DynamicArray *hardQuestions = getQuestionsDifficulty("hard");
@@ -39,27 +49,9 @@ int main(void)
   PriorityQueue *hardPriorityQueue = serializePriorityQueue("hard");
 
   DynamicArray *questionPaper = createDynamicArray(questionCount);
-  for (int i = 0; i < easy; ++i)
-  {
-    Pair *p = popPriorityQueue(easyPriorityQueue);
-    pushDynamicArray(getDynamicArray(p->index, easyQuestions), questionPaper);
-    p->frequency += 1;
-    insertPriorityQueue(easyPriorityQueue, p);
-  }
-  for (int i = 0; i < medium; ++i)
-  {
-    Pair *p = popPriorityQueue(mediumPriorityQueue);
-    pushDynamicArray(getDynamicArray(p->index, mediumQuestions), questionPaper);
-    p->frequency += 1;
-    insertPriorityQueue(mediumPriorityQueue, p);
-  }
-  for (int i = 0; i < hard; ++i)
-  {
-    Pair *p = popPriorityQueue(hardPriorityQueue);
-    pushDynamicArray(getDynamicArray(p->index, hardQuestions), questionPaper);
-    p->frequency += 1;
-    insertPriorityQueue(hardPriorityQueue, p);
-  }
+  addQuestionsToPaper(easyPriorityQueue, easyQuestions, questionPaper, easy);
+  addQuestionsToPaper(mediumPriorityQueue, mediumQuestions, questionPaper, medium);
+  addQuestionsToPaper(hardPriorityQueue, hardQuestions, questionPaper, hard);
 
   for (int i = 0; i < questionPaper->size; ++i)
   {
@@ -78,23 +70,102 @@ int main(void)
       .date = "Today"};
   struct pdf_doc *pdf = pdf_create(PDF_A4_WIDTH, PDF_A4_HEIGHT, &info);
   pdf_set_font(pdf, "Times-Roman");
-  pdf_append_page(pdf);
+  pdf_object *page = pdf_append_page(pdf);
 
-  pdf_add_text(pdf, NULL, "Question Paper", 32, 50, PDF_A4_HEIGHT - 40, PDF_BLACK);
+  pdf_add_text_wrap(
+      pdf, page, "Nirma University",
+      24, 0, PDF_A4_HEIGHT - 40, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_CENTER, NULL);
+  pdf_add_text_wrap(
+      pdf, page, "Institute of Technology",
+      16, 0, PDF_A4_HEIGHT - 54, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_CENTER, NULL);
+  pdf_add_text_wrap(
+      pdf, page, "B. Tech. in Computer Science and Engineering, Semester - III",
+      12, 0, PDF_A4_HEIGHT - 66, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_CENTER, NULL);
+  pdf_add_text_wrap(
+      pdf, page, "2CS301 Data Structures and Algorithms",
+      12, 0, PDF_A4_HEIGHT - 78, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_CENTER, NULL);
+
+  pdf_add_text_wrap(
+      pdf, page, "Roll No:",
+      12, 50, PDF_A4_HEIGHT - 105, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_LEFT, NULL);
+  pdf_add_rectangle(pdf, page, 100, PDF_A4_HEIGHT - 110, 70, 20, 1, PDF_BLACK);
+  pdf_add_text_wrap(
+      pdf, page, "Supervisor's initial with date:",
+      12, PDF_A4_WIDTH - 205, PDF_A4_HEIGHT - 100, 0, PDF_BLACK, 100,
+      PDF_ALIGN_LEFT, NULL);
+  pdf_add_rectangle(pdf, page, PDF_A4_WIDTH - 110, PDF_A4_HEIGHT - 110, 70, 20, 1, PDF_BLACK);
+
+  pdf_add_text_wrap(
+      pdf, page, "Instructions:",
+      12, 50, PDF_A4_HEIGHT - 125, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_LEFT, NULL);
+  pdf_add_text_wrap(
+      pdf, page, "1. Attempt all questions.\n2. Figures to the right indicate difficulty\n3. Use a section-wise separate answer book.\n4. Draw neat sketches wherever necessary.\n5. Assume necessary data wherever required.",
+      12, 120, PDF_A4_HEIGHT - 125, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_LEFT, NULL);
+
+  pdf_set_font(pdf, "Times-Bold");
+  pdf_add_text_wrap(
+      pdf, page, "SECTION - I", 14, 0, PDF_A4_HEIGHT - 200, 0, PDF_BLACK, PDF_A4_WIDTH,
+      PDF_ALIGN_CENTER, NULL);
+  pdf_set_font(pdf, "Times-Roman");
+
   float temp;
-  float gap = 62;
+  float gap = 230;
+  float offset = 20;
+  int index = 0;
   for (int i = 0; i < questionPaper->size; ++i)
   {
-    Question *question = getDynamicArray(i, questionPaper);
+    Question *question = getDynamicArray(index++, questionPaper);
     char *questionText = NULL;
-    asprintf(&questionText, "%d. %s", i + 1, question->question);
+    asprintf(&questionText, "Q.%d %s", index, question->question);
     int y = PDF_A4_HEIGHT - gap;
-    pdf_add_text_wrap(pdf, NULL, questionText, 12, 50, y, 0, PDF_BLACK, PDF_A4_WIDTH - 150, PDF_ALIGN_LEFT, &temp);
-    pdf_add_text(pdf, NULL, question->difficulty, 12, PDF_A4_WIDTH - 50, y, PDF_BLACK);
-    gap += 10 + temp;
-    temp = 0;
+    pdf_add_text_wrap(pdf, page, questionText, 12, 50, y, 0, PDF_BLACK, PDF_A4_WIDTH - 150,
+                      PDF_ALIGN_LEFT, &temp);
+    pdf_add_text(pdf, page, question->difficulty, 12, PDF_A4_WIDTH - 70, y, PDF_BLACK);
+    gap += 6 + temp;
+    if (gap >= PDF_A4_HEIGHT - offset)
+    {
+      gap = offset;
+      page = pdf_append_page(pdf);
+    }
     free(questionText);
+    if (index == easy)
+    {
+      pdf_set_font(pdf, "Times-Bold");
+      pdf_add_text_wrap(
+          pdf, page, "SECTION - II", 14, 0, PDF_A4_HEIGHT - gap, 0, PDF_BLACK, PDF_A4_WIDTH,
+          PDF_ALIGN_CENTER, &temp);
+      pdf_set_font(pdf, "Times-Roman");
+      gap += 12 + temp;
+    }
+    if (index == easy + medium)
+    {
+      pdf_set_font(pdf, "Times-Bold");
+      pdf_add_text_wrap(
+          pdf, page, "SECTION - III", 14, 0, PDF_A4_HEIGHT - gap, 0, PDF_BLACK, PDF_A4_WIDTH,
+          PDF_ALIGN_CENTER, &temp);
+      pdf_set_font(pdf, "Times-Roman");
+      gap += 12 + temp;
+    }
   }
+  // for (int i = 0; i < questionPaper->size; ++i)
+  // {
+  //   Question *question = getDynamicArray(i, questionPaper);
+  //   char *questionText = NULL;
+  //   asprintf(&questionText, "%d. %s", i + 1, question->question);
+  //   int y = PDF_A4_HEIGHT - gap;
+  //   pdf_add_text_wrap(pdf, page, questionText, 12, 50, y, 0, PDF_BLACK, PDF_A4_WIDTH - 150, PDF_ALIGN_LEFT, &temp);
+  //   pdf_add_text(pdf, page, question->difficulty, 12, PDF_A4_WIDTH - 50, y, PDF_BLACK);
+  //   gap += 10 + temp;
+  //   temp = 0;
+  //   free(questionText);
+  // }
 
   char *fileName = NULL;
   asprintf(&fileName, "papers/%s.pdf", pdf_title);
